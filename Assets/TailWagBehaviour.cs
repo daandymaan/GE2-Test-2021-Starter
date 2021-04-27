7b36e71cd73c59b261da7b0b10961cc2530bbc23
Dan Simons <47121977+daandymaan@users.noreply.github.com>
2021-04-27 14:44:52 +0100

    Implemented tail wagging behaviour

---
+++ Assets/TailWagBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TailWagBehaviour : MonoBehaviour
+{
+    public float frequency = 1;
+    public float amplitude = 40;
+    public float theta = 0;
+    void Start()
+    {
+        theta = 0;
+    }
+
+    void Update()
+    {
+        float tailSpeed = transform.parent.gameObject.GetComponent<DogController>().velocity.magnitude;
+        float angle = Mathf.Sin(theta) * amplitude;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.up);
+        transform.localRotation = q;
+        theta += Mathf.PI * 2.0f * Time.deltaTime * frequency * tailSpeed;
+    }
+}
+
