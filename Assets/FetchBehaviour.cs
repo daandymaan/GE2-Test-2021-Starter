f9b84b32b37c73f54024fbabee6ad47de69ddd6d
Dan Simons <47121977+daandymaan@users.noreply.github.com>
2021-04-27 14:01:17 +0100

    Created fetch behaviour whitch fetches a ball

---
+++ Assets/FetchBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FetchBehaviour : SteeringBehaviour
+{
+    public GameObject targetBall;
+    public Vector3 ballPos;
+    public void OnDrawGizmos()
+    {
+        if (Application.isPlaying && isActiveAndEnabled)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawLine(transform.position, ballPos);
+        }
+    }
+
+    void OnEnable()
+    {
+        dog.GetComponent<AudioSource>().Play();
+    }
+
+    public override Vector3 Calculate()
+    {
+        ballPos = targetBall.transform.position;
+        return dog.SeekForce(ballPos);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
